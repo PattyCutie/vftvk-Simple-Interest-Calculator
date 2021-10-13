8981d8b6065676f4314840b536183967fe57a69e
Patty <iamsureiamcute@gmail.com>
2021-10-13 09:40:44 +0000

    change made to simple interest calculator project

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function compute()
-{
-    p = document.getElementById("principal").value;
+{ //create vars and assign values.
+
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years); // year in the future.
     
+    // Get reference to element "result".
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>.\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+    
+    //conditional validation check.
+    if (principal < 1) {
+        alert("Enter a positive number");
+        principal.focus();
+        
+    }
+      
+}
+// fucntion reads the value of the range slider.
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
